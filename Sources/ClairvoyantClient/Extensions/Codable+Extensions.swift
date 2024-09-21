31fcfec8403c81397878774855d24576a0c2fe7a
Christoph Hagen <github@christophhagen.de>
2024-09-21 09:47:06 +0200

    Add Codable extensions

--- Sources/ClairvoyantClient/Extensions/Codable+Extensions.swift
+++ Sources/ClairvoyantClient/Extensions/Codable+Extensions.swift
@@ -6,6 +6,11 @@ extension UnkeyedDecodingContainer {
     mutating func decode<T>() throws -> T where T : Decodable {
         try decode(T.self)
     }
+
+    @inline(__always)
+    mutating func decodeIfPresent<T>() throws -> T? where T : Decodable {
+        try decodeIfPresent(T.self)
+    }
 }
 
 extension KeyedDecodingContainer {
@@ -17,5 +22,14 @@ extension KeyedDecodingContainer {
 
     func decode<T>(forKey key: KeyedDecodingContainer<K>.Key, or defaultValue: T) throws -> T where T : Decodable {
         try decodeIfPresent(T.self, forKey: key) ?? defaultValue
+    }
+}
+
+extension UnkeyedEncodingContainer {
+
+    mutating func encodeIfPresent<T>(_ value: T?) throws where T : Encodable {
+        if let value {
+            try encode(value)
+        }
     }
 }
